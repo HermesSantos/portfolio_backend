80b82d38bbb2a1ad66b8cc7927379ba92f495832
HermesSantos <hermessantos995@gmail.com>
2025-05-01 17:05:47 -0300

    Refactor: Use custom BasicAuth middleware and group routes

---
+++ api/middlewares/authMiddleware.go
@@ -0,0 +1,17 @@
+package middlewares
+
+import (
+	"crypto/subtle"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func BasicAuthMiddleware() echo.MiddlewareFunc {
+	return middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if subtle.ConstantTimeCompare([]byte(username), []byte("hermes")) == 1 &&
+			subtle.ConstantTimeCompare([]byte(password), []byte("123123")) == 1 {
+			return true, nil
+		}
+		return false, nil
+	})
+}
--- api/routes/router.go
+++ api/routes/router.go
@@ -1,31 +1,23 @@
 package routes
 
 import (
-	"crypto/subtle"
 	"log"
 	"net/http"
 	"portfolio_backend/api/handlers/adminHandlers"
-
+	"portfolio_backend/api/middlewares"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 func Router () {
 	e := echo.New()
 
+	// route groups
 	admin := e.Group("/admin")
 	publicAdmin := e.Group("/admin")
 
-	admin.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
-		if subtle.ConstantTimeCompare([]byte(username), []byte("hermes")) == 1 &&
-		subtle.ConstantTimeCompare([]byte(password), []byte("123123")) == 1 {
-			return true, nil
-		}
-		return false, nil
-	}))
+	admin.Use(middlewares.BasicAuthMiddleware())
 
 	publicAdmin.GET("/login", admin_handlers.AdminLogin())
-
 	admin.GET("/", admin_handlers.AdminDash())
 
 	if err := e.Start(":8080"); err != http.ErrServerClosed {
